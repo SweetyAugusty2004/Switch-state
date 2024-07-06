ae9f230371c3654b5ee1e00d65d84b47ded140c4
SweetyAugusty2004 <sweetyaugusty2004@gmail.com>
2024-07-06 20:56:47 +0530

    Add files via upload

---
+++ Switch state/index.js
@@ -0,0 +1,27 @@
+var day= prompt("please enter a day");
+switch(day){
+    case "Monday":
+        alert("Start of the week");
+        break;
+        case "Tuesday":
+        alert("Keep going");
+        break;
+        case "Wednesday":
+        alert("Midweek already");
+        break;
+        case "Thursday":
+        alert("Almost there");
+        break;
+        case "Friday":
+        alert("Last day of the work week");
+        break;
+        case "Saturday":
+        alert("It's the weekend! Enjoy!");
+        break;
+        case "Sunday":
+        alert("Rest & Recharge for the week ahead");
+        break;
+        default:
+        alert("nvalid day entered");
+        break;
+}
